b212d087e0ae0d0f34b4f9016abe0d116dec7c20
Oumarou Bailo Bah <oumaroubailo.bah@monboreal.ca>
2024-10-24 13:46:27 -0400

    Ajout des parametre de la classe carte credit et des achats

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement 
+    {
+
+        public int NumeroCarte { get; set; }
+        public CarteCredit (string descrpition , double montant , int numeroCarte)
+            :base (montant , descrpition)
+        {
+            NumeroCarte = numeroCarte;
+
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le Paiement par Carte de Crédit seleve a un montant de {Montant}, avec une description {Description}, ayant pour Numéro de carte = {NumeroCarte}");
+        }
+    }
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿
+using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit achatN1 = new CarteCredit ("achat de bouquins" ,850.00 , 98754256);
+        CarteCredit achatN2 = new CarteCredit("achat dequipement ", 230.20 , 75456734);
+        CarteCredit achatN3 = new CarteCredit("achat de ressources", 850.00, 42689785);
+
+        achatN1.AfficherDetails();
+        achatN2.AfficherDetails();
+        achatN3.AfficherDetails();
+
+
     }
 }
