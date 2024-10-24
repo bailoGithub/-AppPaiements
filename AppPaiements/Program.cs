d42ac7422ecfca4a19260399a01fe6e448d2ff30
Oumarou Bailo Bah <oumaroubailo.bah@monboreal.ca>
2024-10-24 15:10:11 -0400

    les modif ont reuissie

--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -5,20 +5,28 @@ internal class Program
 {
     private static void Main(string[] args)
     {
+
+
+
+        Utilisateur utilisateur = new Utilisateur("Oumarou Bailo Bah");
+
         CarteCredit achatN1 = new CarteCredit ("achat de bouquins" ,850.00 , 98754256);
         CarteCredit achatN2 = new CarteCredit("achat dequipement ", 230.20 , 75456734);
         CarteCredit achatN3 = new CarteCredit("achat de ressources", 850.00, 42689785);
 
-        achatN1.AfficherDetails();
-        achatN2.AfficherDetails();
-        achatN3.AfficherDetails();
 
-        Paypal paiementN1 = new Paypal (75.00, "Paiement des fourniture de la compta", "bailo@gmail.com");
-        Paypal paiementN2 = new Paypal (30.50, "Paiement du livre de programmation", "oumarou@ahoo.fr");
+        utilisateur.AjouterPaiement(achatN1);
+        utilisateur.AjouterPaiement(achatN2);
+        utilisateur.AjouterPaiement (achatN3);
 
-       
-        paiementN1.AfficherDetails();
-        paiementN2.AfficherDetails();
+        Paypal paiementN1 = new Paypal (99.75, "Paiement des fourniture de la compta", "bailo@gmail.com");
+        Paypal paiementN2 = new Paypal (28.50, "Paiement dun livre de programmation", "oumarou@yahoo.fr");
+
+
+        utilisateur.AjouterPaiement(paiementN1);
+        utilisateur.AjouterPaiement(paiementN2);
+
+        utilisateur.AfficherInfos();
 
 
     }
