937c0ad4c1c79fe139fe305ea4b4ee1350ce2c6d
Oumarou Bailo Bah <oumaroubailo.bah@monboreal.ca>
2024-10-24 14:55:49 -0400

    ajout de la classe USER

--- AppPaiements/Utilisateur.cs
+++ AppPaiements/Utilisateur.cs
@@ -8,5 +8,27 @@ namespace AppPaiements
 {
     internal class Utilisateur
     {
+        public string Nom {  get; set; }
+        public List <Paiement> Paiements { get; set; }
+        public Utilisateur (string nom)
+        {
+
+            Nom = nom;
+            Paiements = new List <Paiement> ();
+        }
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Son Nom est  : {Nom}");
+            Console.WriteLine("la liste des paiements se presentes comme suit :");
+            foreach (var paiement in Paiements)
+            {
+                paiement.AfficherDetails();
+            }
+        }
+
+        public void AjouterPaiement(Paiement paiement)
+        {
+            Paiements.Add(paiement);
+        }
     }
 }
